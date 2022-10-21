ede7cb97f8774a1d95fade74a4fc2ae36052f293
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-10-21 21:52:14 +0700

    Create middleware_app.js
    Creating middleware for secure access from other applications

---
+++ middleware_app.js
@@ -0,0 +1,23 @@
+var appToken = function(req, res, next) {
+       var token = req.header("app_token");
+
+       if(token) {
+              if(token == "TokenKeamanan123") {
+                     next();
+              } else {
+                     res.json({
+                            status:false,
+                            pesan: "app_token TIDAK VALID!",
+                            data:[]
+                     });
+              }
+       } else {
+              res.json({
+                     status:false,
+                     pesan: "Maaf, tidak membawa app_toke",
+                     data:[]
+              });
+       }
+}
+
+module.exports = appToken;  
